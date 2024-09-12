b73939d6c3c7a607eabdb8fe164ae5418916a557
Guillermo Segura <correo@seguraguillermo.net>
2024-09-12 14:02:36 +0100

    feat: update blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,7 @@
+import { BlogCard } from '@/components/BlogCard/BlogCard';
 import { CardHeader, CardSection, CardFooter } from '@/components/Card';
 import { Text } from '@/components/Text/Text';
-import { BlogCard } from '@/components/BlogCard/BlogCard';
-import { joinStringArrays } from '@/utils';
 import content from '@/content/blog.json';
-
-const style = {
-  blogsContainer: [
-    'flex',
-    'flex-col',
-    'space-y-6',
-  ],
-  blog: ['w-full'],
-}
 
 export default function Page() {
   return (
@@ -22,18 +12,15 @@ export default function Page() {
         <Text variant="subtext">{content.content[1]}</Text>
       </CardSection>
 
-      <CardSection>
-        <div className={joinStringArrays(style.blogsContainer)}>
-          {content.articles.map((article) => (
-            <div className={joinStringArrays(style.blog)} key={article.title}>
-              <BlogCard
-                title={article.title}
-                description={article.description}
-                url={article.url}
-              />
-            </div>
-          ))}
-        </div>
+      <CardSection spaceY={6}>
+        {content.articles.map((article) => (
+            <BlogCard
+              key={article.title}
+              title={article.title}
+              description={article.description}
+              url={article.url}
+            />
+        ))}
       </CardSection>
 
       <CardFooter home traits skills />
