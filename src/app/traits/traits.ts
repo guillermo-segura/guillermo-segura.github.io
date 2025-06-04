9e5e96bb07dae0d625353c8c4f8ea117c2a1dec4
Guillermo Segura <35953495+guillermo-segura@users.noreply.github.com>
2025-06-04 10:07:41 +0100

    Fix typos in content

--- src/app/traits/traits.ts
+++ src/app/traits/traits.ts
@@ -2,7 +2,7 @@ const data = {
   preferences: {
     Hobbies: ["🎧 Music", "📖 Books", "🎮 Videogames"],
     Food: ["🌮 Mexican", "🥡 Chinese", "🥘 Mediterranean"],
-    Sports: ["⛰️ Hiking", "👟 Running", "🏓 Table tenis"],
+    Sports: ["⛰️ Hiking", "👟 Running", "🏓 Table tennis"],
   },
 };
 
