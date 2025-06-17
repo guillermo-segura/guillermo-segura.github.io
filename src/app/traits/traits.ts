690c89eecb484225c2cd1ec680df02cc8c868650
Guillermo Segura <35953495+guillermo-segura@users.noreply.github.com>
2025-06-17 10:53:10 +0100

    Update sports preferences

--- src/app/traits/traits.ts
+++ src/app/traits/traits.ts
@@ -2,7 +2,7 @@ const data = {
   preferences: {
     Hobbies: ["🎧 Music", "📖 Books", "🎮 Videogames"],
     Food: ["🌮 Mexican", "🥡 Chinese", "🥘 Mediterranean"],
-    Sports: ["⛰️ Hiking", "👟 Running", "🏓 Table tennis"],
+    Sports: ["⛰️ Hiking", "🧗 Bouldering", "🧘 Yoga"],
   },
 };
 
